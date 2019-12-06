9c661f8b7451563883d3fc984b14e51679c18353
gr683_teo <gr683_teo@L402-3>
2019-12-06 20:45:30 +0700

    gfd

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        return view;
+    }
+}
